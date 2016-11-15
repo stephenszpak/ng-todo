94322b60f6e2ed7b865fbcfdb981cb9d2647734b
Stephen Szpak <srszpak@gmail.com>
2016-11-14 21:30:49 -0600

    day 1 angular

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,41 @@
 "use strict";
 
-console.log("inside app.js")
+var app = angular.module("TodoApp",[]);
 
+app.controller("NavCtrl", function($scope){
+	$scope.navItems = [{name:"Logout"}, {name:"All Items"}, {name:"New Item"}];
+});
 
+app.controller("TodoCtrl", function($scope){
+	$scope.welcome = "hellow"
+	$scope.showListView = true;
+	$scope.items = [
+		{
+			id: 0,
+			task: "mowtheloawn",
+			isCompleted: true,
+			assignedTo: "Zoe"
+		},
+		{
+			id: 1,
+			task: "blow the lawn",
+			isCompleted: false,
+			assignedTo: "William"
+		},
+		{
+			id: 2,
+			task: "bunch of stuff",
+			isCompleted: false,
+			assignedTo: "Zoe"
+		}
+	];
+
+	$scope.allItems = function() {
+		console.log("you clicks all itemds");
+		$scope.showListView = true;
+	}
+	$scope.newItem = function() {
+		console.log("you click new items");
+		$scope.showListView = false;
+	}
+});
