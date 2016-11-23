2963812a0636c6d2566b2d06ba8058a35db2e039
Stephen Szpak <srszpak@gmail.com>
2016-11-22 19:18:52 -0600

    more auth

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -1,8 +1,19 @@
 "use strict";
 
-app.controller("AuthCtrl", function($scope, AuthFactory, UserFactory) {
+app.controller("AuthCtrl", function($scope, $rootScope, $location, AuthFactory, UserFactory) {
 	$scope.loginContainer = true;
 	$scope.registerContainer = false;
+
+	let logMeIn = function(loginStuff) {
+		AuthFactory.authenticate(loginStuff).then(function(didLogin) {
+			return UserFactory.getUser(didLogin.uid);
+		}).then(function(userCreds) {
+			$rootScope.user = userCreds;
+			$scope.login = {};
+			$scope.register = {};
+			$location.url("/items/list");
+		});
+	};
 
 	$scope.setLoginContainer = function() {
 		$scope.loginContainer = true;
@@ -19,14 +30,12 @@ app.controller("AuthCtrl", function($scope, AuthFactory, UserFactory) {
 			registerNewUser.uid = didRegister.uid;
 			return UserFactory.addUser(registerNewUser);
 		}).then(function(registerComplete) {
-			//LOGIN
+			logMeIn(registerNewUser);
 		});
 	};
 
 	$scope.loginUser = function(loginNewUser) {
-		AuthFactory.authenticate(loginNewUser).then(function(didLogin) {
-			console.log("didLogin", didLogin);
-		});
+		logMeIn(loginNewUser);
 	};
 
 });
--- javascripts/factories/UserFactory.js
+++ javascripts/factories/UserFactory.js
@@ -19,9 +19,10 @@ app.factory("UserFactory", function($q, $http, FIREBASE_CONFIG) {
 		});
 	};
 
+
 	let getUser = (userId) => {
 		return $q((resolve, reject) => {
-			$http.get(`${FIREBASE_CONFIG.databaseURL}/user.json?orderBy="uid"&equalTo="${userId}"`)
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/users.json?orderBy="uid"&equalTo="${userId}"`)
 			.success(function(userObject) {
 				let users = [];
 				Object.keys(userObject).forEach(function(key) {
